854b38405c0a33b622e21b88b7bb09a19d71c350
Carlos T <carlesbergersen@gmail.com>
2023-03-31 15:31:40 +0200

    improvement: prepare multiple layouts

--- pages/index.js
+++ pages/index.js
@@ -19,23 +19,21 @@ export default function Home () {
     setMaterialsList(materials)
   }
 
-  const [layoutImg, setLayoutImg] = useState('')
+  const [layoutImgMap, setLayoutImgMap] = useState({})
 
   return (
     <div className='flex min-h-screen flex-col items-center justify-center py-2'>
       <div style={{ position: 'relative' }}>
-        <div>
-          <img src={'https://firebasestorage.googleapis.com/v0/b/visualizer-new-devs-test.appspot.com/o/base.jpeg?alt=media&token=358ccdea-3cf9-4751-ae48-4631e4700554'} />
-          {layoutImg && <img src={layoutImg}
-            style={{ position: 'absolute', zIndex: '2', top: '0' }}
+        <img src={'https://firebasestorage.googleapis.com/v0/b/visualizer-new-devs-test.appspot.com/o/base.jpeg?alt=media&token=358ccdea-3cf9-4751-ae48-4631e4700554'} />
+        {pointsList.map((point, i) => {
+          return <div key={point._id}>
+          {layoutImgMap[point._id] && <img src={layoutImgMap[point._id]}
+            style={{ position: 'absolute', zIndex: `3.${i}`, top: '0' }}
           />}
-
-        </div>
-        {pointsList.map(point => {
-          return <div key={point._id}
+          <div
             onClick={() => loadMaterials(point._id)}
             style={{
-              zIndex: '3',
+              zIndex: '4',
               position: 'absolute',
               top: `${point.coordY}%`,
               left: `${point.coordX}%`,
@@ -43,17 +41,20 @@ export default function Home () {
             }}>
               <img width={35} height={35} src={'https://cdn-icons-png.flaticon.com/512/890/890122.png'} />
             </div>
+          </div>
         })}
 
         <div
           style={{
-            zIndex: '3',
+            zIndex: '5',
             position: 'absolute',
             top: 0,
             right: 0
           }}>
           {materialsList.map((material, i) => {
-            return <div key={i} onClick={() => setLayoutImg(material.layers[material.points[0]])}>
+            const pointId = material.points[0]
+            const materialUrl = material.layers[pointId]
+            return <div key={i} onClick={() => setLayoutImgMap(prev => ({ ...prev, [pointId]: materialUrl }))}>
               <div>{material.name}</div>
               <div><img src={material.materialPreview} /></div>
             </div>
