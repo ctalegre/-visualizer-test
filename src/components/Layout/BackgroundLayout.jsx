39aeab764be716fef401f93198356d345d3683e4
Carlos T <carlesbergersen@gmail.com>
2023-04-01 23:49:59 +0200

    chore: add background

---
+++ src/components/Layout/BackgroundLayout.jsx
@@ -0,0 +1,10 @@
+function BackgroundLayout () {
+  const BACKGROUND_URL = 'https://firebasestorage.googleapis.com/v0/b/visualizer-new-devs-test.appspot.com/o/base.jpeg?alt=media&token=358ccdea-3cf9-4751-ae48-4631e4700554'
+  return (
+    <img
+      src={BACKGROUND_URL}
+    />
+  )
+}
+
+export default BackgroundLayout
