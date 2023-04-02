fda6f536e32fc84071e23d170b0e313c579f5890
Carlos T <carlesbergersen@gmail.com>
2023-04-02 20:09:43 +0200

    chore: sidebar styles

--- src/components/Material/Material.jsx
+++ src/components/Material/Material.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+// import { useDispatch, useSelector } from 'react-redux'
+
 function Material (props) {
+  const { material, onClick } = props
   return (
-    <div>Material</div>
+    <div onClick={onClick} className='my-3 flex rounded-md border-4 border-[#f5f2ef] bg-[#f5f2ef] shadow-md'>
+      <div style={{
+        color: '#7a7676',
+        // background: '#f5f2ef',
+        // fontWeight: '100'
+      }} className='flex w-36 items-center justify-center text-sm text-[#7a7676]'>{material.name}</div>
+      <div className=''><img className='h-24 w-24' src={material.materialPreview} /></div>
+    </div>
   )
 }
 
--- src/components/Material/SideBarMaterials.jsx
+++ src/components/Material/SideBarMaterials.jsx
@@ -1,6 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { applyMaterialByPointId } from '../../redux/reducers/materialsSlice'
+import Material from './Material'
+
 function SideBarMaterials (props) {
+  const dispatch = useDispatch()
+
+  const materialList = useSelector((state) => state.materialsSlice.materialList)
+  // const isLoadingMaterialList = useSelector((state) => state.pointsSlice.isLoading)
+
+  function applyMaterial (material) {
+    const pointId = material.points[0]
+    const materialUrl = material.layers[pointId]
+    dispatch(applyMaterialByPointId({ pointId, image: materialUrl }))
+  }
+
   return (
-    <div>SideBarMaterials</div>
+    <div className='fixed right-0 top-0 z-[5] flex flex-col items-end'>
+      {materialList.map((material, i) => <Material
+        key={i}
+        onClick={() => applyMaterial(material)}
+        material={material}
+      />)}
+    </div>
   )
 }
 
